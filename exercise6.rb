c7050bba415377b927d75a27dc359d66b8f2677f
RHEA GANAPATHY <rheasg@gmail.com>
2015-06-10 16:31:48 -0400

    completed exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -7,6 +7,7 @@ end
 
 # Method to print grocery list
 def prints_grocery_list(list)
+	puts "-----"
 	list.each do |item|
 		puts "* #{item}"
 		
@@ -14,5 +15,31 @@ def prints_grocery_list(list)
 end
 
 grocery_list = add_item_to_list("apples", grocery_list)
+
+prints_grocery_list(grocery_list)
+
+puts grocery_list.length
+
+if grocery_list.include?("banana") == false
+	puts "You need to pick up bananas"
+else puts "You don't need to pick up bananas"
+end
+
+puts grocery_list[1]
+
+# Method to sort items on grocery list alphabetically
+def sort_list_alphabetically(list)
+	list.sort.each {|item| puts "* #{item}"}
+end
+
+sort_list_alphabetically(grocery_list)
+
+# Method to delete item of list
+def delete_item_from_list(item, list)
+	list.delete(item)
+	return list
+end
+
+delete_item_from_list("laundry detergent", grocery_list)
 
 prints_grocery_list(grocery_list)
